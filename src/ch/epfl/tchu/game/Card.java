8238038da31518659965e85f3d87126e96ebfa15
TheAmericanMomo <aymericdechillaz@gmail.com>
2021-02-23 20:21:40 +0100

    classe Trip

--- src/ch/epfl/tchu/game/Card.java
+++ src/ch/epfl/tchu/game/Card.java
@@ -17,12 +17,22 @@ public enum Card {
 
     public static final List<Card> ALL = List.of(Card.values());
     public static final int COUNT = Card.ALL.size();
-
+    public final Color couleur;
+    
+    /*
+    private Card () {}
+    
+    private Card (Color couleur){
+        this.couleur = couleur;
+    }
+    
 //    final Color type;
 //    Card(Color type) { this.type = type; }
 
-    public static final List<Card> CARS = List.of(Card.values()).remove(List.of(Card.values()).size() - 1);   //A REVOIR
-
+  //  public static final List<Card> CARS = List.of(Card.values()).remove(List.of(Card.values()).size() - 1);   //A REVOIR
+  //  public static final List<Card> CARS = Card.values().remove(Card.LOCOMOTIVE);
+    
+    */
     /**
      * @param color
      * @return the type of wagon card corresponding to a color
