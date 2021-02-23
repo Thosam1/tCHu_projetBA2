df459472354b94b37286ee43959077661be6e553
Thosam1 <63594070+Thosam1@users.noreply.github.com>
2021-02-23 22:35:18 +0100

    initializing

--- src/ch/epfl/tchu/game/Card.java
+++ src/ch/epfl/tchu/game/Card.java
@@ -5,38 +5,43 @@ import java.util.List;
 
 public enum Card {
 
-    BLACK,
-    VIOLET,
-    BLUE,
-    GREEN,
-    YELLOW,
-    ORANGE,
-    RED,
-    WHITE,
-    LOCOMOTIVE;
+    BLACK(Color.BLACK),      // wagons
+    VIOLET(Color.VIOLET),
+    BLUE(Color.BLUE),
+    GREEN(Color.GREEN),
+    YELLOW(Color.YELLOW),
+    ORANGE(Color.ORANGE),
+    RED(Color.RED),
+    WHITE(Color.WHITE),
+    LOCOMOTIVE(null); // has to be at the end - null
 
     public static final List<Card> ALL = List.of(Card.values());
     public static final int COUNT = Card.ALL.size();
 
-//    final Color type;
-//    Card(Color type) { this.type = type; }
+    final private Color color;
 
-    public static final List<Card> CARS = List.of(Card.values()).remove(List.of(Card.values()).size() - 1);   //A REVOIR
+    private Card(Color color) { this.color = color;}  // if null -> locomotive
+
+    public static final List<Card> CARS = List.of(Card.values()).subList(0, COUNT-1);   //A REVOIR
 
     /**
      * @param color
      * @return the type of wagon card corresponding to a color
      */
     public static Card of(Color color) {
-        return null;
+        if(color == null) {
+            return Card.valueOf(LOCOMOTIVE.toString());
+        }else{
+            return Card.valueOf(color.toString());
+        }
     }
 
     /**
      * @return
      */
-    public static Color color() {
-        return null;
-    }
+    public Color color() {
+        return color;
+    }   // null if locomotive
 
     //test is this working ?
 }
--- src/testing.java
+++ src/testing.java
@@ -6,12 +6,20 @@ public class testing {
 
     public static void main(String[] args) {
 
+        System.out.println();
         System.out.println("- - - - - C O L O R S - - - - -");
         System.out.println("All colors list : " + Color.ALL);
         System.out.println("All colors count : " + Color.COUNT);
+        System.out.println();
 
         System.out.println("- - - - - C A R D - - - - -");
         System.out.println("All cards list : " + Card.ALL);
         System.out.println("All cards count : " + Card.COUNT);
+        System.out.println("All cards color : " + Card.CARS);
+
+        System.out.println("This is a blue card/wagon : " + Card.of(Color.BLUE) + " and this is a locomotive : " + Card.of(null));
+        System.out.println("The blue color of this card is : " + Card.BLUE.color());;
+        System.out.println("The color of the locomotive card is : " + Card.LOCOMOTIVE.color());
+
     }
 }
