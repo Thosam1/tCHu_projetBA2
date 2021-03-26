f4338691f13f7d372c6b0a80e80e2131549f1bea
Thosam1 <63594070+Thosam1@users.noreply.github.com>
2021-03-26 11:55:37 +0100

    ...

--- src/ch/epfl/tchu/game/Game.java
+++ src/ch/epfl/tchu/game/Game.java
@@ -40,7 +40,7 @@ public final class Game {
      * @param rng
      * @throws IllegalArgumentException si l'une des deux tables associatives a une taille différente de 2
      */
-    public static void play(Map<PlayerId, Player> players, Map<PlayerId, String> playerNames, 
+    public static void play(Map<PlayerId, Player> players, Map<PlayerId, String> playerNames,
         SortedBag<Ticket> tickets, Random rng) {
         Preconditions.checkArgument((players.size()==2)&&(playerNames.size()==2));
         int numberOfLastTurns = 0; //égal à 1 quand l'avant dernier joueur joue et 2 qand le dernier joue
@@ -74,8 +74,8 @@ public final class Game {
             c.getValue().setInitialTicketChoice(gameState.topTickets(Constants.INITIAL_TICKETS_COUNT));    // les deux joueurs pourront donc consulter leur choix en parallèle, et même utiliser l'interface graphique
             gameState = gameState.withoutTopTickets(Constants.INITIAL_TICKETS_COUNT);
         }
-            /*infoMap.forEach((playerId, playerInfo) -> 
-            v.receiveInfo(playerInfo.drewTickets(Constants.INITIAL_TICKETS_COUNT)));
+            /*infoMap.forEach((playerId, playerInfo) ->
+            v.receiveInfo(playerInfo.drewTickets(Constants.INITIAL_TICKETS_COUNT)));        // on est pas censé appeler ceci
             Il faut attendre que tous les joueurs aient fait leur choix comme dit
             à la deuxieme ligne sur quand receiveInfo doit etre appelé*/    // -> c'est fait plus bas
 
@@ -91,7 +91,7 @@ public final class Game {
             c.getValue().updateState(gameState, gameState.playerState(PlayerId.PLAYER_2));
         }
         for(Map.Entry<PlayerId, Player> c : players.entrySet()){
-            SortedBag<Ticket> chosenTickets = c.getValue().chooseInitialTickets();
+            SortedBag<Ticket> chosenTickets = c.getValue().chooseInitialTickets();  // pop up
             gameState.withInitiallyChosenTickets(c.getKey(), chosenTickets);  // faut-il mettre ceci ici ? OUI
             c.getValue().updateState(gameState, gameState.playerState(c.getKey())); // gameState ? faut-il le transformer en publicGameState ? NON, ça joue comme ça
         }
@@ -103,18 +103,18 @@ public final class Game {
         /*players.forEach((c, v) -> { // après leur choix, on les informe
         // joueur 1 reçoit l'info de l'adversaire seulement ou de lui-même aussi ? bonne question
         // c'est bien chooseInitialTickets() de la classe Player qu'il faut appeler ?
-        // je dirai que oui  
+        // je dirai que oui
             v.receiveInfo(player1.keptTickets(players.get(PlayerId.PLAYER_1).chooseInitialTickets().size()));
             v.receiveInfo(player2.keptTickets(players.get(PlayerId.PLAYER_2).chooseInitialTickets().size()));
         });*/
         for(Map.Entry<PlayerId, Player> c : players.entrySet()){ // après leur choix, on les informe -> équitable
-            c.getValue().receiveInfo(player1.keptTickets(gameState.playerState(PlayerId.PLAYER_1).ticketCount())); // joueur 1 reçoit l'info de l'adversaire seulement ou de lui-même aussi ? Oui !
+            c.getValue().receiveInfo(player1.keptTickets(gameState.playerState(PlayerId.PLAYER_1).ticketCount())); // chaque joueur reçoit les infos des deux joueurs ?
             c.getValue().receiveInfo(player2.keptTickets(gameState.playerState(PlayerId.PLAYER_2).ticketCount())); // c'est bien chooseInitialTickets() de la classe Player qu'il faut appeler ? Non !
 
         }
-        
-        
-        
+
+
+
         /**La partie commence*/
         while(numberOfLastTurns<=2){//le loop s'arrete quand les deux joueurs ont joué une dernière fois
             //currentPlayerInterface
@@ -134,12 +134,12 @@ public final class Game {
              */
 
             Player.TurnKind turnKind = currPlayerInterf.nextTurn();
-            
-            
+
+
             if(turnKind == Player.TurnKind.DRAW_TICKETS) {
                 //faut il verifier qu il reste des tickets
                 SortedBag<Ticket> drawnTickets = gameState.topTickets(Constants.IN_GAME_TICKETS_COUNT);
-                
+
                 players.forEach((c, v) -> {
                     v.receiveInfo(currInf.drewTickets(Constants.IN_GAME_TICKETS_COUNT));   // Avant qu'il choisisse
                 });
@@ -160,7 +160,7 @@ public final class Game {
                     //Constans.DECK_SLOT est égal à -1 et signigie que le joueur veut la carte du haut du deck
                     gameState = (slot == Constants.DECK_SLOT) ? gameState.withBlindlyDrawnCard() : gameState.withDrawnFaceUpCard(slot);
 
-                    for(Map.Entry<PlayerId, Player> c : players.entrySet()){ // if a visible card, what about discards ???
+                    for(Map.Entry<PlayerId, Player> c : players.entrySet()){ // if a visible card, what about discards ??? Pouvons-nous piocher de la défausse ?
                         if ((slot == Constants.DECK_SLOT)) {
                             c.getValue().receiveInfo(currInf.drewBlindCard());
                         } else {
@@ -170,7 +170,7 @@ public final class Game {
                     // faut il communiquer ceci à l'autre joueur ?
                     if(i == 0){ // entre le premier et le deuxième tirage
                         for(Map.Entry<PlayerId, Player> c : players.entrySet()){
-                            c.getValue().updateState(gameState, gameState.currentPlayerState());    // aussi celui de l'adversaire ?
+                            c.getValue().updateState(gameState, gameState.currentPlayerState());    // aussi celui de l'adversaire ? de quelle façon ?
 //                            v.updateState(gameState, gameState.playerState(PlayerId.PLAYER_1));
 //                            v.updateState(gameState, gameState.playerState(PlayerId.PLAYER_2));
                         }
@@ -181,66 +181,66 @@ public final class Game {
             // gameState
             // receive info
             // update state
-            
+
             else {  //comment vérifier que le joueur a assez de wagons
                 /**la route que le player veut*/
                 Route routeDésiré = currPlayerInterf.claimedRoute();
                 SortedBag<Card> initialCards = currPlayerInterf.initialClaimCards();
-                
+
                 if (routeDésiré.level() == Route.Level.UNDERGROUND) {
                     SortedBag.Builder builder = new SortedBag.Builder<>();
-                    
+
                     for(int i = 0; i<Constants.ADDITIONAL_TUNNEL_CARDS; ++i) {
                         gameState = gameState.withCardsDeckRecreatedIfNeeded(rng);
                         builder.add(gameState.topCard());
                         gameState = gameState.withoutTopCard(); //retourne un nouveau gameState sans la carte du haut
                         }
-                    SortedBag<Card> drawnCards = builder.build(); 
+                    SortedBag<Card> drawnCards = builder.build();
 
                     //Les trois cartes piochés sont rajoutés à la défausse
                     gameState = gameState.withMoreDiscardedCards(drawnCards);
-                    
+
                     int additionalCardsCount = routeDésiré.additionalClaimCardsCount(initialCards, drawnCards);
-                    
+
                     List<SortedBag<Card>> possibleAdditionalCards = gameState.currentPlayerState()
                             .possibleAdditionalCards(additionalCardsCount, initialCards, drawnCards);
-                    
-                    
+
+
                     if((additionalCardsCount>=1) && (additionalCardsCount<=3) && (possibleAdditionalCards.size()!=0)) {
-                        SortedBag<Card> additionalCards = currPlayerInterf.chooseAdditionalCards(possibleAdditionalCards);   
+                        SortedBag<Card> additionalCards = currPlayerInterf.chooseAdditionalCards(possibleAdditionalCards);
                         //Est ce que chooseAdditionalCards retourne toutes les cartes à utiliser ou seulement les cartes additionelles?
                         gameState = (additionalCards.size()==0)? gameState :  gameState.withClaimedRoute(routeDésiré, initialCards.union(additionalCards)) ;
                     }
-                    
+
                     else if(additionalCardsCount==0) {
                         gameState = gameState.withClaimedRoute(routeDésiré, initialCards);
-                        
+
                     }
-                    
+
                     else {
                         //la route n est pas rajouté car le joueur n a pas les cartes additionelles
-                        
+
                     }
                 }
                 else {
                     gameState = gameState.withClaimedRoute(routeDésiré, initialCards);
                 }
             }
-            
+
             gameState = gameState.forNextTurn();
-            
+
             //incrémente numberOfLastTurns pour qu il y ait deux turns une fois que lastTurnBegins rend true
             if((gameState.lastTurnBegins())||(numberOfLastTurns !=0)) {
                 ++numberOfLastTurns;
             }
         }
-        
+
         int player1Score = gameState.playerState(PlayerId.PLAYER_1).finalPoints();
         int player2Score = gameState.playerState(PlayerId.PLAYER_2).finalPoints();
-        
+
         int player1LongestTrail = Trail.longest(gameState.playerState(PlayerId.PLAYER_1).routes()).length();
         int player2LongestTrail = Trail.longest(gameState.playerState(PlayerId.PLAYER_2).routes()).length();
-        
+
         PlayerId playerLongestTrail;
         switch(Integer.compare(player1LongestTrail, player2LongestTrail)) {
         case 1:
@@ -259,7 +259,7 @@ public final class Game {
         default:
             break;
         }
-        
+
         PlayerId playerWinner;
         switch(Integer.compare(player1Score, player2Score)) {
         case 1:
@@ -274,7 +274,7 @@ public final class Game {
         default:
             break;
         }
-       
+
     }
 
 
