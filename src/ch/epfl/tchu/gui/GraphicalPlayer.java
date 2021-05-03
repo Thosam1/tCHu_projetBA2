9ffccf884c3e69918c32e2fdf2a0d34e22da3edc
Thosam1 <63594070+Thosam1@users.noreply.github.com>
2021-05-03 14:29:20 +0200

    graphical

--- src/ch/epfl/tchu/gui/GraphicalPlayer.java
+++ src/ch/epfl/tchu/gui/GraphicalPlayer.java
@@ -1,5 +1,6 @@
 package ch.epfl.tchu.gui;
 
+import java.awt.*;
 import java.util.List;
 import java.util.Map;
 
@@ -18,11 +19,23 @@ import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.Property;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.ObservableList;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import javax.swing.text.html.ListView;
 
 public final class GraphicalPlayer {
     private ObservableGameState observableGame;
     private ObservableList<Text> messageList;
+    private final PlayerId playerId;
+    private final Map<PlayerId, String> mapPlayerNames;
     
     //propriétés contenant les gestionnaires d'action
     //si elle contient null, alors l'action en question est actuellement interdite
@@ -31,7 +44,8 @@ public final class GraphicalPlayer {
     private ObjectProperty<ChooseTicketsHandler> chooseTicketsProperty = new SimpleObjectProperty<>();
     
     public GraphicalPlayer(PlayerId playerId, Map<PlayerId, String> mapPlayerNames) {
-        
+        this.playerId = playerId;
+        this.mapPlayerNames = mapPlayerNames;
     }
     
     public void setState(PublicGameState newGameState, PlayerState newPlayerState) {
@@ -76,4 +90,42 @@ public final class GraphicalPlayer {
     }
     
     public void choosedAdditionalCards(List<SortedBag<Card>> possibleAdditionalCards, ChooseCardsHandler chooseCardsHandler) {}
+
+    private Stage mainSceneGraph(Node mapView, Node drawView, Node handView, Node infoView){
+        BorderPane borderPane = new BorderPane(mapView, null, drawView, handView, infoView);
+        Stage root = new Stage();
+        root.setScene(new Scene(borderPane));
+        root.setTitle("tCHu" + " - " + mapPlayerNames.get(playerId));
+        root.show();    //todo show-void or return ?
+        return root;
+    }
+
+
+    public static Stage chooseGraph(String title, String introText, String number, ObservableList list){  // titre est donné soit par la constante TICKETS_CHOICE, soit par la constante CARDS_CHOICE de StringsFr
+        Stage stage = new Stage(StageStyle.UTILITY);
+        stage.initOwner();  //todo ? soit la fenêtre principale de l'interface (créée par le constructeur de GraphicalPlayer)
+        stage.initModality(Modality.WINDOW_MODAL);
+
+        VBox vBox = new VBox();
+
+        Text text = new Text(number == null ? introText : String.format(StringsFr.CHOOSE_TICKETS, number)); //toDo mieux de formater en dehors ou dedans ?
+        TextFlow textFlow = new TextFlow(text);
+
+
+        ListView listView = new ListView(list);
+        listView.
+
+        Button button = new Button();
+
+        vBox.getChildren().addAll(textFlow, listView, button);
+
+        Scene scene = new Scene(vBox);
+        scene.getStylesheets().add("chooser.css");
+        stage.setTitle(title);      //depends
+        stage.setScene(scene);
+
+
+        return stage;
+    }
+
 }
