59d026d009260e24eb2d7361458d1dfdb37a0cd6
TheAmericanMomo <aymericdechillaz@gmail.com>
2021-05-03 11:15:06 +0200

    InfoViewCreator

---
+++ src/ch/epfl/tchu/gui/InfoViewCreator.java
@@ -0,0 +1,98 @@
+package ch.epfl.tchu.gui;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import ch.epfl.tchu.game.PlayerId;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.StringExpression;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Separator;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+
+class InfoViewCreator {
+    public VBox vBox;
+    
+    /**
+     * Le seul attribut de la classe est une instance de VBox
+     * */
+    private InfoViewCreator(VBox vBox) {
+        this.vBox = vBox;
+    }
+    
+    
+    public static VBox createInfoView(PlayerId playerId, 
+            Map<PlayerId, String> mapPlayerNames,
+            ObservableGameState observableGame,
+            ObservableList<Text> messageList) {
+        
+        VBox vBox = new VBox();
+        vBox.getStylesheets().addAll("info.css", "colors.css");
+        
+        Separator separator = new Separator();//séparateur orienté horizontalement
+        
+        /**Statistiques joueurs*/
+        VBox vBoxPlayerStats = new VBox();
+        vBoxPlayerStats.setId("player-stats");
+        vBox.getChildren().add(vBoxPlayerStats);
+        
+        List<PlayerId> playerIdList = new ArrayList<>(playerId, playerId.next());
+        
+        for(PlayerId player : playerIdList) {
+            TextFlow textFlowPlayerStatistics = new TextFlow();
+            vBoxPlayerStats.getChildren().add(textFlowPlayerStatistics);//est ce qu il faut rajouter le joueur courant en premier pour qu il soit placé en haut
+            
+            textFlowPlayerStatistics.getStyleClass()
+            .add(player.toString());
+            
+            Circle circle = new Circle(5);//il faudra peut etre le placer
+            circle.getStyleClass().add("filled");
+            
+            Text textPlayerStatistics = new Text();
+            
+            StringExpression stringExpression = Bindings.format(StringsFr.PLAYER_STATS, mapPlayerNames.get(player), observableGame.nbTicketsInHand(player),
+                    observableGame.nbCardsInHand(player), observableGame.nbCarsInHand(player), observableGame.nbConstructionPoints(player));
+            
+            textPlayerStatistics.textProperty().bind(stringExpression);
+            
+            textFlowPlayerStatistics.getChildren().addAll(circle, textPlayerStatistics);
+        }
+        
+        
+        /*for (int i=0; i<mapPlayers.size(); ++i) {
+            TextFlow textFlowPlayerStatistics = new TextFlow();
+            vBoxPlayerStats.getChildren().add(textFlowPlayerStatistics);//est ce qu il faut rajouter le joueur courant en premier pour qu il soit placé en haut
+            
+            String playerIndex = String.valueOf(i+1);
+            textFlowPlayerStatistics.getStyleClass()
+            .add(String.join("_", List.of("PLAYER", playerIndex)));
+            
+            Circle circle = new Circle(5);//il faudra peut etre le placer
+            circle.getStyleClass().add("filled");
+            
+            Text textPlayerStatistics = new Text();
+            
+            StringExpression stringExpression = Bindings.format(StringsFr.PLAYER_STATS, );
+            //Comment accéder aux valeurs à passer? probablement a partir de la map
+            textPlayerStatistics.textProperty().bind(stringExpression);
+            
+            textFlowPlayerStatistics.getChildren().addAll(circle, textPlayerStatistics);
+        }*/
+        
+        /**Messages*/
+        TextFlow textGameInfo = new TextFlow();
+        textGameInfo.setId("game-info");
+        Bindings.bindContent(textGameInfo.getChildren(), messageList);
+        
+        
+        
+        vBox.getChildren().addAll(vBoxPlayerStats, separator, textGameInfo);
+        
+        return vBox;
+    }
+   }
